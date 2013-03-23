26e5833e86ddd97168397395d110a9504a185754
Van Michael <van.michael.nguyen@gmail.com>
2013-03-22 23:27:45 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OffTheCobApp::Application.config.secret_token = '5281a7f7fa82f52ea534e51da18dff591069d4fe29a7dded1d648db8248fa71bacfd942b24e97fa62f939f89a9c1f591348b0fa068d21ec197bf458e0820ecae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OffTheCobApp::Application.config.session_store :cookie_store, key: '_off_the_cob_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OffTheCobApp::Application.config.session_store :active_record_store
