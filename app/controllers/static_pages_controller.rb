1eef559164b27857666598fc4c165a5594accca5
Van Michael <van.michael.nguyen@gmail.com>
2013-04-26 21:49:45 -0400

    test

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,23 @@
+class StaticPagesController < ApplicationController
+
+	def home
+	end
+
+	def our_mission
+	end
+
+	def members
+	end
+
+	def contact_info
+	end
+
+	def directions
+	end
+		
+	def speakers
+	end
+
+	def networking
+	end
+end
